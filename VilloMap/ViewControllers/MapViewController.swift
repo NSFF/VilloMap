489aebe9809529464b881f27e15f60e61f13fb34
rvancraenenbroek <robin.van.craenenbroek@student.ehb.be>
2022-06-06 22:22:37 +0200

    Trying to get the formatting of Villo data to JSON but Failing

--- VilloMap/ViewControllers/MapViewController.swift
+++ VilloMap/ViewControllers/MapViewController.swift
@@ -15,6 +15,8 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     
     var locationManager = CLLocationManager()
     
+    var hondentoiletten:[Hondentoilet]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -27,6 +29,109 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
             locationManager.startUpdatingLocation()
         }
         
+        getDataFromWebservice()
+        
+    }
+    
+
+    func getDataFromWebservice() -> Void {
+        let url = URL(string: "https://data.mobility.brussels/geoserver/bm_bike/wfs?service=wfs&version=1.1.0&request=GetFeature&typeName=bm_bike:villo&outputFormat=json&srsName=EPSG:4326")!
+        let task = URLSession.shared.dataTask(with: url) {
+        (data: Data?, response: URLResponse?, error: Error?) -> Void in
+            
+            if let jsonData = data
+            {
+                let decoder = JSONDecoder()
+
+                do {
+                    /*self.hondentoiletten = try decoder.decode([Hondentoilet].self, from: jsonData)
+                   */
+                    
+                    self.translateData(responseData: jsonData)
+                    
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+            DispatchQueue.main.async {
+                //self.addAnnotations()
+            }
+        }
+        task.resume()
+    }
+    
+    func translateData(responseData:Data)
+    {
+        let json = try! JSONSerialization.jsonObject(with: responseData, options: []) as? [String: AnyObject]
+        
+        //let list = json!["features"] as! [AnyObject]
+        //let bbox = list.forEach{print($0)}
+        //let bbox = list[0]
+        //print(bbox)
+        /*let today = Date()
+        var date1 = Date()
+        var dayCounter = 0
+     
+        var newDay = false
+        var iconURLS = [String]()
+        for counter in 0...list.count-1{
+            let items = list[counter] as! [String: AnyObject]
+            var date2 = Date()
+            
+            if  let timeResult = (items["dt"] as? Double) {
+                date2 = Date(timeIntervalSince1970: timeResult)
+            }
+            
+            
+            if Calendar.current.compare(date1, to: date2, toGranularity: .day) != ComparisonResult.orderedSame {
+                if (newDay){
+                    dayCounter += 1
+                }
+                newDay = true
+            }
+            if dayCounter == 3 {
+                return
+            }
+            if Calendar.current.compare(today, to: date2, toGranularity: .day) != ComparisonResult.orderedSame
+            {
+                let main = items["main"] as! [String: AnyObject]
+                
+                let temp = main["temp"] as! Double
+                let tempMin = main["temp_min"] as! Double
+                let tempMax = main["temp_max"] as! Double
+                let weatherArray = items["weather"] as! [AnyObject]
+                let weather = weatherArray[0] as! [String: AnyObject]
+                let icon = weather["icon"] as! String
+                let iconURL = URL(string: "http://openweathermap.org/img/w/\(icon).png")
+                if !iconURLS.contains(icon) {
+                    WeatherInfoSingleton.shared.downloadImage(url: URL(string: iconURL!.absoluteString)!)
+                }
+               iconURLS.append(icon)
+                WeatherInfoSingleton.shared.weatherInfos.append([WeatherInfo]())
+                WeatherInfoSingleton.shared.weatherInfos[dayCounter].append(WeatherInfo(date: date2, temp: temp, tempMin: tempMin, tempMax: tempMax, icon: iconURL!.absoluteString))
+                
+            }
+            
+           
+            
+            date1 = date2
+        }*/
+        
+    }
+
+    func addAnnotations() -> Void {
+        var annotations = [MKAnnotation]()
+
+        for toilet in self.hondentoiletten!
+        {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate.latitude = toilet.coordinaat.lat
+            annotation.coordinate.longitude = toilet.coordinaat.lon
+            annotation.title = toilet.adres
+            annotations.append(annotation)
+        }
+        mapView.addAnnotations(annotations)
+        
     }
     
     
@@ -37,7 +142,7 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         
         mapView.setRegion(region, animated: true)
     }
-
+    
 
 }
 
